e5f9407b55467d01dd4ce1d9cca90ea1492217fb
NovgorodovMP <90719126+NovgorodovMP@users.noreply.github.com>
2021-10-09 09:32:01 +0300

    Update main.cpp

--- Homework_3_task_3/main.cpp
+++ Homework_3_task_3/main.cpp
@@ -19,10 +19,11 @@ int main()
         ++counter;
         if (N == 0)
         {
-            numberNew = counter > 1 ? (numberNew + number / pow(10, counter - 1)) : number;
+            
             break;
         }
     }
+    numberNew = counter > 1 ? (numberNew + number / pow(10, counter - 1)) : number;
     numberNew += counter > 1 ? (number % 10 * pow(10, counter - 1)) : 0;
     number /= 10;
     for (int i = 1; i < counter - 1; ++i)
